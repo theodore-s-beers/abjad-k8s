6ae4e132aeb13d8fa5011b42afd8ff6cf5ed5025
Theo Beers <tb3367@drexel.edu>
2024-09-03 15:46:52 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+#![warn(clippy::pedantic, clippy::nursery)]
+
+use abjad::{Abjad, AbjadPrefs};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Params {
+    input: String,
+}
+
+#[get("/")]
+async fn index(params: web::Query<Params>) -> impl Responder {
+    let abjad = params.input.abjad(AbjadPrefs::default());
+    HttpResponse::Ok().body(abjad.to_string())
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
+}
